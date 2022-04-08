81900f1b46edba97fec0b5212f92830281dd0d56
olgabs <93311817+olgabs@users.noreply.github.com>
2022-04-07 23:46:47 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		
+		Cachorro Dog = new Cachorro("Loki",2,"latido","Muito","Shi-tsu","Macho");
+		Cavalo Cav = new Cavalo("Pé de pano",8,"Som de cavalo padrao","Mais ainda","Quarto de milha","Macho","Branco");
+		Preguiça Pre = new Preguiça("Preguiça",5,"????","Nope","Femea","Caramelo");
+		
+		
+		
+		
+		Dog.InfoC();
+		System.out.println("\n");
+		Cav.InfoE();
+		System.out.println("\n");
+		Pre.InfoP();
+		
+		
+		
+		
+		
+
+	}
+
+}
